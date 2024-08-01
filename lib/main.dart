930caf35c7417687f22acfd9a37f680103308f66
verma <malpotra8082@gmail.com>
2024-08-01 17:26:50 +0530

    Added splash_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:test_app/firebase_options.dart';
-import 'package:test_app/login_page.dart';
 import 'package:test_app/provider/auth_provider.dart';
 import 'package:test_app/provider/theme_provider.dart';
+import 'package:test_app/splash_screen.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
         title: 'Test_App',
 
       theme: themeProvider.currentTheme,
-      home: LoginPage(),
+      home: SplashScreen(),
 
        );
       },
